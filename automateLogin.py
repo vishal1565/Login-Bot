a2188593d2fa0f99d3ce22a5dbf7a8a061b25e24
Vishal Vishwakarma <vishal.1565@gmail.com>
2019-04-12 13:10:17 +0530

    added fb

---
+++ automateLogin.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+import sys
+
+def printUse():
+    print("Usage: python automateLogin.py -[fb/gmail]")
+    print("Eg: python automateLogin.py -fb")
+    print("Eg: python automateLogin.py -gmail")
+
+if len(sys.argv)==2:
+    email = input("Enter Email: ")
+    psd = input("Enter Password: ")
+    
+    if sys.argv[1]=='-gmail':
+        print("Now logging you in your gmail account..\n")
+        driver = webdriver.Chrome()
+        driver.get("https://mail.google.com")
+        driver.find_element_by_id('identifierId').send_keys(email)
+        driver.find_element_by_id('identifierNext').click()
+        sleep(2)
+        driver.find_element_by_name('password').send_keys(psd)
+        driver.find_element_by_id('passwordNext').click()
+        driver.maximize_window()
+    
+    else:
+        print("Unsupported website")
+else:
+    printUse()
