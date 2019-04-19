1664e17a28f30263c29573b36bded103827ece20
Vishal Vishwakarma <vishal.1565@gmail.com>
2019-04-19 11:04:59 +0530

    Update automateLogin.py

--- automateLogin.py
+++ automateLogin.py
@@ -13,23 +13,29 @@ if len(sys.argv)==2:
     psd = input("Enter Password: ")
 
     if sys.argv[1]=='-gmail':
-        print("Now logging you in your gmail account..\n")
-        driver = webdriver.Chrome()
-        driver.get("https://mail.google.com")
-        driver.find_element_by_id('identifierId').send_keys(email)
-        driver.find_element_by_id('identifierNext').click()
-        sleep(2)
-        driver.find_element_by_name('password').send_keys(psd)
-        driver.find_element_by_id('passwordNext').click()
-        driver.maximize_window()
+        try:
+            print("Now logging you in your gmail account..\n")
+            driver = webdriver.Chrome()
+            driver.get("https://mail.google.com")
+            driver.find_element_by_id('identifierId').send_keys(email)
+            driver.find_element_by_id('identifierNext').click()
+            sleep(2)
+            driver.find_element_by_name('password').send_keys(psd)
+            driver.find_element_by_id('passwordNext').click()
+            driver.maximize_window()
+        except:
+            print("An Error Occurred..")
     elif sys.argv[1]=='-fb':
-        print("Logging you to your facebook id..\n")
-        driver = webdriver.Chrome()
-        driver.get("https://facebook.com")
-        driver.find_element_by_id('email').send_keys(email)
-        driver.find_element_by_id('pass').send_keys(psd)
-        driver.find_element_by_id('loginbutton').click()
-        driver.maximize_window()
+        try:
+            print("Logging you to your facebook id..\n")
+            driver = webdriver.Chrome()
+            driver.get("https://facebook.com")
+            driver.find_element_by_id('email').send_keys(email)
+            driver.find_element_by_id('pass').send_keys(psd)
+            driver.find_element_by_id('loginbutton').click()
+            driver.maximize_window()
+        except:
+            print("An Error Occurred..")
     else:
         print("Unsupported website")
 else:
